65fe25f425c694cafef73947d8c41280852a8059
Dennis Ruppel <crap-dennis@gmx.net>
2025-12-16 22:48:52 +0100

    try out uv

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="WebNovelScrapper",
+    version='0.1',
+    description="Let's you download Japanese web novels from syosetu and kakuyomu",
+    author="Dennis Ruppel",
+    packages=find_packages(),
+)
